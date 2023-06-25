ca0f1cbe9ecdc669992e6d93288b193b4a6d9a1f
ISHAN KOUL <ishankoul02@gmail.com>
2023-06-25 10:41:42 +0000

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+
+let maximum=parseInt(prompt("enter the maximum number"));
+while(!maximum){
+maximum= parseInt(prompt("please enter a valid number"));
+}
+
+const targetNum= Math.floor(Math.Random* maximum)+1;
+
+let guess=parseInt("enter the guess");
+let attempts=1;
+while(parseInt(guess)!=targetNum){
+    attempts++;
+    if(guess === 'q'){
+        break;
+    }
+    if(guess>targetNum){
+        guess= (prompt("too high,try again"));
+    }
+    if(guess<targetNum){
+        guess= (prompt("too low,try again"));
+    }
+}
+if(guess === 'q'){
+    console.log("quitting");
+}
+console.log(`you got it. it took you ${attempts} guesses!`);
+
