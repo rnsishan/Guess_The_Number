722b87e5336214b1009ddffeebe6edd45da8ce84
ISHAN KOUL <ishankoul02@gmail.com>
2023-06-25 16:28:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ while (parseInt(guess) !== targetNum) {
 if (guess === 'q') {
     console.log("OK, YOU QUIT!")
 } else {
-    console.log("CONGRATS YOU WIN!")
-    console.log(`You got it! It took you ${attempts} guesses`)
+    alert"CONGRATS YOU WIN!")
+    alert(`You got it! It took you ${attempts} guesses`)
 }
